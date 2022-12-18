9607e6cdce3945bd98e85425b358cd8878bb77a4
MilotoiuMihail <milotoiu.mihail1@gmail.com>
2022-12-18 22:17:35 +0200

    binded elements

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,20 +1,46 @@
 window.onload = function () {
-    navigator.geolocation.getCurrentPosition((position) => {
-        onLocationAccess(position)
+    let mapElement = document.getElementById('map')
+    let btnAllow = document.getElementById('btnAllow')
+    let btnSet = document.getElementById('btnSet')
+    let title = document.getElementById('title')
+    let pickupElement = document.getElementById('pickup')
+    let destinationElement = document.getElementById('destination')
+    let currentMarker = null
+    let map = null
+    let canSet = false
+
+    initMap(37.7749, -122.4194) //setting coordinates for Uber HQ
+
+    btnAllow.addEventListener('click', () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            onLocationAccess(position)
+        })
     })
 
-    function onLocationAccess(position) {
-        let mapElement = document.getElementById('map');
+    btnSet.addEventListener('click', () => {
+        canSet = true
+    })
 
-        let currentMarker = null
+    pickupElement.addEventListener('focus', () => {
+        title.textContent = 'Where can we pick you up?'
+    })
 
+    destinationElement.addEventListener('focus', () => {
+        title.textContent = 'Where to?'
+    })
+
+    pickupElement.addEventListener('keypress', () => {
+
+    })
+
+    function initMap(lat, lng) {
         let verticalBound = mapElement.offsetHeight * .5;
         let bounds = L.latLngBounds(L.latLng(-verticalBound, 300), L.latLng(verticalBound, -300));
-
-        let map = L.map('map', {
+        map = L.map('map', {
+            zoom: 10,
             maxBounds: bounds,
             worldCopyJump: true
-        }).setView([position.coords.latitude, position.coords.longitude], 15)
+        }).setView([lat, lng], 13)
 
         L.tileLayer('https://api.maptiler.com/maps/bright-v2/256/{z}/{x}/{y}.png?key=jZkKIah32WCkciSEKI2d', {
             minZoom: 4,
@@ -24,10 +50,16 @@ window.onload = function () {
 
         map.zoomControl.setPosition('bottomright')
 
+
         map.addEventListener('click', (e) => {
+            if (!canSet) return
             if (currentMarker)
                 currentMarker.removeFrom(map)
             currentMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map)
         })
+    }
+
+    function onLocationAccess(position) {
+        map.setView([position.coords.latitude, position.coords.longitude], 17)
     }
 }
